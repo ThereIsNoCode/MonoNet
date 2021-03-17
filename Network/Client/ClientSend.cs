d5064cbf74748dcbbd2b7baae3eda0b6a2f03093
ThereIsNoCode <50684592+ThereIsNoCode@users.noreply.github.com>
2021-03-17 19:24:08 -0400

    Update ClientSend.cs

--- Network/Client/ClientSend.cs
+++ Network/Client/ClientSend.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MonoNet.Packets;
 
-namespace MonoTestInput
+namespace MonoNet.Client
 {
     class ClientSend
     {
