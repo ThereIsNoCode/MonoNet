02da1d8be8ea8ac0e253191eddf70120f0f8bcdc
ThereIsNoCode <50684592+ThereIsNoCode@users.noreply.github.com>
2021-03-17 19:25:32 -0400

    Update Packet.cs

--- Network/Server/Packet.cs
+++ Network/Server/Packet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MonoTestInput
+namespace MonoNet.Packets
 {
     /// <summary>Sent from server to client.</summary>
     public enum ServerPackets
